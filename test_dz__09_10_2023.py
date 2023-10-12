482aa98ad6e821f811e4675cd2beb7781e571640
anatt <anattoliy966@gmail.com>
2023-10-12 18:08:26 +0300

    inint dz__09_10_2023

---
+++ test_dz__09_10_2023.py
@@ -0,0 +1,15 @@
+# dz__09_10_2023
+
+# class TestDz:
+def test_1():
+    result = 5 ** 2  # Возводим 5 в квадрат
+    expected_result = 25
+    assert result == expected_result
+
+def test_2():
+    num1 = 10
+    num2 = 5
+    result = num1 + num2
+    expected_result = 15
+    assert result == expected_result
+
